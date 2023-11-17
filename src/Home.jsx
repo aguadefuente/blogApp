5ca622408ffb8f89562d7b5eac7a065b551b3f46
lenilaurae <lenilaurae.gmail.com>
2023-11-17 15:29:35 +0100

    estudio

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,7 +3,7 @@ import useFetch from "./useFetch";
 
 const Home = () => {
    const {data: blogs, isPending, error} = useFetch("http://127.0.0.1:8000/blogs/") //aquí determinamos el valor de la url
-   //renombremos la property de data a blogs para que en este caso tenga que ver con nuestra aplicación que es de un Blog
+   //renombramos la property de data a blogs para que en este caso tenga que ver con nuestra aplicación que es de un Blog
 
     return ( 
     <div className="home">
@@ -13,12 +13,12 @@ const Home = () => {
         {/*&& operator se asegura que una vez la data llegue se renderice - sino estíamos haciendo un map sobre un null que es el valor inicial del useState blogs */}
         {blogs && <Bloglist blogprop={blogs} titleprop="All Blogs"></Bloglist>}
         
-        {/*Volveos a usar el componente Bloglist pero con otra data - 
-        usaremos filter() para que traiga sólo los blogs escritos por mario - descomentar para ver que renderiza
+        {/*Para ejercitar el filter() methos: vamos a usar el componente Bloglist nuevamente pero que traiga 
+        sólo los blogs escritos por mario - descomentar para ver que renderiza
         
         {blogs && <Bloglist blogprop={blogs.filter((blog) => blog.author === "mario")} titleprop="Mario`s Blog"></Bloglist>}
-        
         */}
+
     </div>
     );
 }
